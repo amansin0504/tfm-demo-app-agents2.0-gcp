73aaeb9ba362b7644dbf72504dd7312e1ed6c296
Amandeep Singh <amansin3@cisco.com>
2024-10-10 19:10:28 -0400

    script update to new url

--- scripts/ad.sh
+++ scripts/ad.sh
@@ -4,10 +4,9 @@ sudo apt install python3-pip -y
 sudo apt install wget
 sudo pip install Flask
 
-git clone https://github.com/amansin0504/tfm-cldemo-app-gcp-vm.git
-mkdir app/
-mkdir app/templates
-cp tfm-cldemo-app-gcp-vm/source/ad.py app/app.py
-cp tfm-cldemo-app-gcp-vm/source/templates/ad.json app/templates/
+git clone https://github.com/amansin0504/tfm-demo-app-agents2.0-gcp.git
+mkdir -p app/templates
+cp tfm-demo-app-agents2.0-gcp/source/ad.py app/app.py
+cp tfm-demo-app-agents2.0-gcp/source/templates/ad.json app/templates/
 cd app
 flask run  --host=0.0.0.0 -p 8990&
--- scripts/email.sh
+++ scripts/email.sh
@@ -4,10 +4,9 @@ sudo apt install python3-pip -y
 sudo apt install wget
 sudo pip install Flask
 
-git clone https://github.com/amansin0504/tfm-cldemo-app-gcp-vm.git
-mkdir app/
-mkdir app/templates
-cp tfm-cldemo-app-gcp-vm/source/email.py app/app.py
-cp tfm-cldemo-app-gcp-vm/source/templates/email.json app/templates/
+git clone https://github.com/amansin0504/tfm-demo-app-agents2.0-gcp.git
+mkdir -p app/templates
+cp tfm-demo-app-agents2.0-gcp/source/email.py app/app.py
+cp tfm-demo-app-agents2.0-gcp/source/templates/email.json app/templates/
 cd app
 flask run  --host=0.0.0.0 -p 8993&
