2fb584b8b4a21efee3d471c30b9d62e9401b455e
Amandeep Singh <amansin3@cisco.com>
2024-10-10 22:35:20 -0400

    variable clean up

--- variables.tf
+++ variables.tf
@@ -35,39 +35,3 @@ variable dbsubnet2 {
     type = string
     default = "10.120.216.0/24"
 }
-
-#if you change cidr block make sure you update addresses in python and shell scripts.
-variable frontendip {
-    default = "10.120.211.10"
-}
-variable checkoutip {
-    default = "10.120.213.10"
-}
-variable adip {
-    default = "10.120.213.11"
-}
-variable recommendationip {
-    default = "10.120.213.12"
-}
-variable paymentip {
-    default = "10.120.213.13"
-}
-variable emailip {
-    default = "10.120.213.14"
-}
-variable productcatalogip {
-    default = "10.120.213.15"
-}
-variable shippingip {
-    default = "10.120.213.16"
-}
-variable currencyip {
-    default = "10.120.213.17"
-}
-variable cartip {
-    default = "10.120.213.18"
-}
-variable redisip {
-    default = "10.120.215.10"
-}
-
