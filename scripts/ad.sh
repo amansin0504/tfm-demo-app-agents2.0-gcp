8bf7f6e3db7d3a4f13eb5b56bcbca769a661a50c
Amandeep Singh <amansin3@cisco.com>
2024-10-17 18:11:04 -0400

    unzip install

--- scripts/ad.sh
+++ scripts/ad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install python3-pip -y
-sudo apt install wget
+sudo apt install wget unzip
 sudo pip install Flask
 
 git clone https://github.com/amansin0504/tfm-demo-app-agents2.0-gcp.git
@@ -13,4 +13,4 @@ flask run  --host=0.0.0.0 -p 8990&
 
 wget -O csw-linux-installer.sh "${downloadurl}"
 chmod 755 csw-linux-installer.sh 
-sudo ./csw-linux-installer.sh
+
--- scripts/shipping.sh
+++ scripts/shipping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install python3-pip -y
-sudo apt install wget
+sudo apt install wget unzip
 sudo pip install Flask
 
 git clone https://github.com/amansin0504/tfm-demo-app-agents2.0-gcp.git
